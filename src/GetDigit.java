829d512a9b28f1cfdce00787f176b94bb045b1ee
alokl <aloktech.lohani@gmail.com>
2021-03-14 12:05:24 +0530

    First commit

---
+++ src/GetDigit.java
@@ -0,0 +1,22 @@
+
+public class GetDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "1alok7lohani is 8good";
+		String result ="";
+		         
+		   for (int i=0; i<str.length(); i++){
+		             
+		   Character chars = str.charAt(i);
+		    if(Character.isDigit(chars))
+		     {
+		          result += chars;
+		       }
+		 }
+		System.out.println(result);
+
+	}
+
+}
